2c934c15120cf59feda72c4c4da0b9f52e98e0fa
sugitamato <sugitamato1234@gmail.com>
2019-03-30 03:13:57 +0900

    update20190330

--- js/all.js
+++ js/all.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+$(function(){
   /*-- モーダルウィンドウ --*/
   $('.inline').colorbox({
     inline: true,
@@ -16,9 +16,6 @@ document.addEventListener('DOMContentLoaded', function(){
     overlay_opacity: '0.8',
     overlay_close: false,
   });
-  });
-$(function(){
-
 
   /*-- モーダルウィンドウ　背景画像_各曲 --*/
   var bgImg = $('.modal_img_frame img');
